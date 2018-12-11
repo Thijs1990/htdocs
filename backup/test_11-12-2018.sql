c8261c43937b33e0d7168da7c70b2191fea9018e
Thijs Ouwens <thijsouwens@hotmail.com>
2018-12-11 17:39:13 +0100

    added login
    Added a simple login form.
    Removed useless table from test.

---
+++ backup/test_11-12-2018.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2018 at 05:37 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_email` varchar(40) NOT NULL,
+  `user_pass` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_email`, `user_pass`, `user_name`) VALUES
+(4, 'dddddddddddddddddddddddddddddddddddddddd', 'fgcgfcgfcgf', 'trdrtdsrtstrstrs'),
+(5, '', '', ''),
+(6, 'gfjhtfu@yjtyjtdyt.gjfyir', 'kjgfkuygf', 'kjyfuyf'),
+(7, 'test@test.nl', 'drgdrg', 'Test');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_email` (`user_email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ login.php
@@ -0,0 +1,9 @@
+
+
+<form action="index.php" method="post">
+  <p>Input email address: <input type="email" name="user_email" placeholder="enter your email address" required></p>
+  <p>Input password: <input type="password" name="user_pass" placeholder="" required></p>
+</form>
+
+
+//de input van dit formulier checken tegen test/user, daar moet binnen een row dezelfde email - pass combinatie staan
